10549e588ee9792ca64415b748fa4adcf6ddfe3a
mattyu123 <matthewyu100@gmail.com>
2023-09-07 23:44:22 -0400

    implemented useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,25 @@
+import { useState } from "react"
+
 const useVisualMode = (initial) => {
-  const state = initial
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return (
-    { mode: state }
-  )
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory([initial])
+    }
+    setHistory(prev => [...prev, newMode])
+  }
 
+  function back() {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    }
+  }
+
+  return { mode: history[history.length -1], transition, back };
 }
 
+
 export default useVisualMode;
+
